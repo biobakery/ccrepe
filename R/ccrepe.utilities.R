7f30f1547d37e018b7e4669ae2d4d15b5bc344ad
George_Weingart <George_Weingart@localhost>
2013-10-07 19:56:21 -0400

    ccrepe: Modified apply(x, 1, sum) to use rowSums and added to DESCRIPTION Suggests: knitr, BiocStyle

--- R/ccrepe.utilities.R
+++ R/ccrepe.utilities.R
@@ -39,7 +39,7 @@ function(data){
 #*************************************************************************************
 #* A function to normalize a data matrix across the rows                             *
 #*************************************************************************************
-	data.normalized <- data/apply(data,1,sum)		#Normalize
+	data.normalized <- data/rowSums(data)		#Normalize
 	data.normalized[is.na(data.normalized )] <- 0 #Replace NA's with Zeros
   	return(data.normalized)
 }
@@ -833,7 +833,7 @@ function(X,CA)
 			ErrMsg = paste('Not enough data - found ',nrow(mydata),' rows of data - Less than  ',CA$min.subj, ' (=min.subj) - Run Stopped')  #Error 
 			stop(ErrMsg)
 			}
-		ProcessedX = mydata/apply(mydata,1,sum)
+		ProcessedX = mydata/rowSums(mydata)
 	return(ProcessedX)
 }
 
