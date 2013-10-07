546df188f762c4d75fb9fb56873bf7e4f369ca98
eschwager <emma.schwager@gmail.com>
2013-10-07 17:27:06 -0400

    reverting changes temporarily

--- R/ccrepe.R
+++ R/ccrepe.R
@@ -14,8 +14,7 @@ function(
 	errthresh = 0.0001, 				#Threshold error if there is enough data to calculate cor an pval for certain i and k - For first dataset
 	verbose = FALSE,					#Request for verbose output
 	iterations.gap = 100,				#If output is verbose - after how many iterations issue a status message?
-	distributions = NA,					#Output Distribution file - For ccrepe
-	compare.within.x=TRUE				#Boolean of whether to do comparisons within subset.cols.x or between subset.cols.x and subset.cols.y
+	distributions = NA					#Output Distribution file - For ccrepe
 	)					
 
 {
@@ -30,7 +29,6 @@ function(
 			iterations=iterations,
 			subset.cols.1=subset.cols.x,
 			subset.cols.2=subset.cols.y,
-			compare.within.x=compare.within.x,
 			errthresh=errthresh,
 			method=sim.score,
 			method.args=sim.score.args,
